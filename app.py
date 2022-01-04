b2b3e8839efd1184b75f426d311869894b3d8e56
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-01-04 13:26:29 +0100

    feat: create the postgresql database model

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "postgresql://localhost/height_collector"
+db = SQLAlchemy(app)
+
+
+class Data(db.Model):
+    __tablename__ = "data"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+    height = db.Column(db.Integer)
+
+    def __init__(self, email, height):
+        self.email = email
+        self.height = height
 
 
 @app.route("/")
